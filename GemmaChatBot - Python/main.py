372f32f9fe8070404b5ee89f46bf608b14e593c0
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2024-02-23 13:40:52 +0100

    Make chatbot chatable

--- GemmaChatBot - Python/main.py
+++ GemmaChatBot - Python/main.py
@@ -4,15 +4,27 @@ from datetime import datetime
 
 start = datetime.now()
 
+print("Device name:", torch.cuda.get_device_properties('cuda').name)
+print("FlashAttention available:", torch.backends.cuda.flash_sdp_enabled())
+print(f'torch version: {torch.version}')
+
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 model = AutoModelForCausalLM.from_pretrained(
     "google/gemma-2b-it",
     torch_dtype=torch.float16,
-).to("cuda")
+    device_map="auto"
+)
 
-input_text = "Write me a poem about Machine Learning."
-input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+conversation_history = []
 
-outputs = model.generate(**input_ids, max_new_tokens=4000)
-print(tokenizer.decode(outputs[0]))
-print(datetime.now() - start)
+while True:
+    history_string = "\n".join(conversation_history)
+
+    input_text = input("> ")
+
+    input_ids = tokenizer.encode_plus(history_string, input_text, return_tensors="pt").to("cuda")
+
+    outputs = model.generate(**input_ids, max_new_tokens=1000, pad_token_id=tokenizer.eos_token_id)
+
+    response = tokenizer.decode(outputs[0])
+    print(response)
